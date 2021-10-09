a11c555a0e4dc6504f07f91fa6c668de4325ceda
Sandro Bezerra <alexsandro.g.bezerra@gmail.com>
2021-10-09 09:51:01 -0300

    refactor: remove unusued files

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hello World Page</title>
+      </Head>
+
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+    </>
+  );
+}
